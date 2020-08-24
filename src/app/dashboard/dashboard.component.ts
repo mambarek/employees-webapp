a34b2bb9a9e7b2b8a572e15056b5441ee72dc810
Mbarek, Mohamed Ali <mbarek@it-2go.de>
2020-08-24 15:01:26 +0200

    Editing project and save changes works well.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProjectControllerService} from '@angular-it2go/project-management-api';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  projectsCount = 0;
 
-  ngOnInit(): void {
+  constructor(private projectControllerService: ProjectControllerService) {
   }
 
+  ngOnInit(): void {
+    this.initView();
+  }
+
+  initView(): void {
+    this.projectControllerService.getProjectsCount().subscribe(result => this.projectsCount = result,
+      error => console.error(error));
+  }
 }
