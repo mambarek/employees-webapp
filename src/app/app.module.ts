d106536ce12bbe360a2c9a52465d1b210192b591
Mbarek, Mohamed Ali <mbarek@it-2go.de>
2020-08-24 11:27:46 +0200

    Editing project and save changes works well.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
-import { EditProjectComponent } from './projects/edit-project/edit-project.component';
-import { ProjectListComponent } from './projects/project-list/project-list.component';
+import {MenuComponent} from './menu/menu.component';
+import {ProjectDetailsComponent} from './projects/project-details/project-details.component';
+import {ProjectListComponent} from './projects/project-list/project-list.component';
 import { EmployeesListComponent } from './employees/employees-list/employees-list.component';
 import { EditEmployeeComponent } from './employees/edit-employee/edit-employee.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -14,20 +14,22 @@ import { EditCarComponent } from './cars/edit-car/edit-car.component';
 import { CarListComponent } from './cars/car-list/car-list.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { ApiModule as ProjectsApiModule } from '@angular-it2go/project-management-api';
+import {ApiModule as ProjectsApiModule} from '@angular-it2go/project-management-api';
+import {ErrorComponent} from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     MenuComponent,
-    EditProjectComponent,
+    ProjectDetailsComponent,
     ProjectListComponent,
     EmployeesListComponent,
     EditEmployeeComponent,
     DashboardComponent,
     EditCarComponent,
-    CarListComponent
+    CarListComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
