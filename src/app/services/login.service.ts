63523ae15813d513a6b365ea99567f3f7681d799
Mohamed Ali Mbarek <mbarek@it-2go.de>
2021-02-15 22:23:11 +0100

    new version

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -3,6 +3,19 @@ import { Injectable } from '@angular/core';
 import {AuthConfig, OAuthService} from "angular-oauth2-oidc";
 import {environment} from "../../environments/environment";
 
+/**
+ * The OAuth Login uses "angular-oauth2-oidc" vom Manfred Steyer
+ * Single-page apps
+ * Cannot securely store a Client Secret because their entire source is available to the browser.
+ * Given these situations, OAuth 2.0 provides a version of the Authorization Code Flow which makes
+ * use of a Proof Key for Code Exchange (PKCE) (defined in OAuth 2.0 RFC 7636).
+ * The PKCE-enhanced Authorization Code Flow introduces a secret created by the calling application
+ * that can be verified by the authorization server; this secret is called the Code Verifier.
+ * Additionally, the calling app creates a transform value of the Code Verifier called
+ * the Code Challenge and sends this value over HTTPS to retrieve an Authorization Code.
+ * This way, a malicious attacker can only intercept the Authorization Code, and they cannot
+ * exchange it for a token without the Code Verifier.
+ */
 @Injectable({
   providedIn: 'root'
 })
